e0ce29767ea76cfeea1bb65a7636806b07302d2e
Ganesh <realganu@outlook.com>
2025-03-12 19:58:40 +0530

    first commit

---
+++ project/components/footer.php
@@ -0,0 +1,38 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 9699972931</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +91 9371214647</a>
+         <a href="mailto:realganu@outlook.com"><i class="fas fa-envelope"></i> realganu@outlook.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Pune Maharashtra, India - 411038</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My Bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">Facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">Twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">Instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">Linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">Youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; Copyright @ 2025 by Hotel and Resorts | All rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
---
+++ project/components/user_header.php
@@ -0,0 +1,23 @@
+<!-- header section starts  -->
+
+<section class="header">
+
+   <div class="flex">
+      <a href="#home" class="logo">Hotels and Resorts</a>
+      <a href="#availability" class="btn">Check Availability</a>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </div>
+
+   <nav class="navbar">
+      <a href="index.php#home">Home</a>
+      <a href="index.php#about">About</a>
+      <a href="index.php#reservation">Reservation</a>
+      <a href="index.php#gallery">Gallery</a>
+      <a href="index.php#contact">Contact</a>
+      <a href="index.php#reviews">Reviews</a>
+      <a href="bookings.php">My Bookings</a>
+   </nav>
+
+</section>
+
+<!-- header section ends -->
